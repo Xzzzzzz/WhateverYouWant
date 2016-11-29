ccfa206d40501c7ca5ecce4ba487d3a1ac74a399
IVAN Z <IVAN Z>
2016-11-29 14:21:07 -0800

    Signed-off-by: IVAN Z <IVAN Z>

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,20 +2,39 @@ var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     console.log("Hello World from controller");
 
-
 var refresh = function() {
-  $http.get('/contactlist').success(function(response) {
-    console.log("I got the data I requested");
-    $scope.contactlist = response;
-    $scope.contact = "";
+  $http.get('/resume').success(function(response) {
+    var length = Object.keys(response).length;
+    console.log(length);
+    var education =[], skill = [];
+    var project = [];
+    for(var i=0;i<length;i++)
+    {
+      if(response[i].type === 'edu'){
+        education.push(response[i]);
+       $scope.education = education;
+      }
+      if(response[i].type === 'project'){
+        project.push(response[i]);
+       $scope.project = project;
+      }
+      else if(response[i].type === 'skill'){
+        skill.push(response[i]);
+       $scope.skill = skill;
+      }
+     
+    }
   });
 };
 
+
+
 refresh();
 
-$scope.addContact = function() {
-  console.log($scope.contact);
-  $http.post('/contactlist', $scope.contact).success(function(response) {
+$scope.addSchool = function() {
+  console.log($scope.edu.toSource());
+  $scope.edu.type = 'edu';
+  $http.post('/addSchool', $scope.edu).success(function(response) {
     console.log(response);
     refresh();
   });
@@ -23,21 +42,21 @@ $scope.addContact = function() {
 
 $scope.remove = function(id) {
   console.log(id);
-  $http.delete('/contactlist/' + id).success(function(response) {
+  $http.delete('/resume/' + id).success(function(response) {
     refresh();
   });
 };
 
 $scope.edit = function(id) {
   console.log(id);
-  $http.get('/contactlist/' + id).success(function(response) {
+  $http.get('/resume/' + id).success(function(response) {
     $scope.contact = response;
   });
 };  
 
 $scope.update = function() {
   console.log($scope.contact._id);
-  $http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response) {
+  $http.put('/resume/' + $scope.contact._id, $scope.contact).success(function(response) {
     refresh();
   })
 };
--- server.js
+++ server.js
@@ -3,55 +3,63 @@
 var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
-var db = mongojs('contactlist', ['contactlist']);
+var db = mongojs('resume', ['resume']);
 var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
-app.get('/contactlist', function (req, res) {
+app.get('/resume', function (req, res) {
   console.log('I received a GET request');
 
-  db.contactlist.find(function (err, docs) {
+  db.resume.find(function (err, docs) {
     console.log(docs);
     res.json(docs);
+    console.log("response sent");
   });
+  
 });
 
-app.post('/contactlist', function (req, res) {
-  console.log(req.body);
-  db.contactlist.insert(req.body, function(err, doc) {
+app.post('/addSchool', function(req, res){
+  db.resume.insert(req.body, function(err,doc){
     res.json(doc);
   });
 });
 
-app.delete('/contactlist/:id', function (req, res) {
+app.post('/resume', function (req, res) {
+  console.log("request body"+req.body);
+  db.resume.insert(req.body, function(err, doc) {
+    res.json(doc);
+  });
+});
+
+app.delete('/resume/:id', function (req, res) {
   var id = req.params.id;
   console.log(id);
-  db.contactlist.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
+  db.resume.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
     res.json(doc);
   });
 });
 
-app.get('/contactlist/:id', function (req, res) {
+app.get('/resume/:id', function (req, res) {
   var id = req.params.id;
   console.log(id);
-  db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
+  db.resume.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
     res.json(doc);
   });
 });
 
-app.put('/contactlist/:id', function (req, res) {
+app.put('/resume/:id', function (req, res) {
   var id = req.params.id;
-  console.log(req.body.name);
-  db.contactlist.findAndModify({
+  console.log(req.body.school);
+  db.resume.findAndModify({
     query: {_id: mongojs.ObjectId(id)},
-    update: {$set: {name: req.body.name, email: req.body.email, number: req.body.number}},
+    update: {$set: {school: req.body.school, skill: req.body.skill, description: req.body.description}},
     new: true}, function (err, doc) {
       res.json(doc);
     }
   );
 });
 
-app.listen(3000);
-console.log("Server running on port 3000");
+app.listen(8080);
+console.log("Server running on default port 8080");
